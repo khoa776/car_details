8ee79d04a0fa88f22b8d8e0fdd3d5353962795f4
khoa <khoa776lt@gmail.com>
2026-01-31 15:45:02 +0700

    Completed clean

--- src/clean.py
+++ src/clean.py
@@ -1,24 +1,51 @@
 import pandas as pd
 
 def extract_numeric(df):
-    df['max_power'] = df['max_power'].str.extract(r'(\d+)\s*bhp')       #()=>BAT GIA TRI, \d =>0-9, + =>NHIEU SO
+    df['max_power'] = df['max_power'].str.extract(r'([\d\.]+)\s*bhp')       #()=>BAT GIA TRI, \d =>0-9, + =>NHIEU SO
     df['engine'] = df['engine'].str.extract(r'(\d+)\s*CC')              #\s =>KHOANG TRANG, * => CO HOAC KHONG
-    df['mileage'] = df['mileage'].str.extract(r'(\d+)\s*kmpl|km/kg')
-    #df['mileage'] = df['mileage'].str.extract(r'([\d\.]+)') ==> CHI BAT SO
+    df[['mileage','mileage_unit']] = df['mileage'].str.extract(r'([\d\.]+)\s*(kmpl|km/kg)')             #==> CHI BAT SO
+    return df
+
+def change_types(df):
+    df['max_power'] = pd.to_numeric(df['max_power'], errors='coerce')
+    df['engine'] = pd.to_numeric(df['engine'], errors='coerce')
+    df['mileage'] = pd.to_numeric(df['mileage'], errors='coerce')
+    df['year'] = pd.to_numeric(df['year'],errors='coerce')
+    df['seats'] = pd.to_numeric(df['seats'], errors='coerce')
+    return df
 
 def handle_missing(df):
     df['torque'] = df['torque'].fillna('unknown')
+    df['seats'] = df['seats'].fillna(df['seats'].mode()[0])
 
-    important_columns = ['mileage', 'engine', 'max_power', 'torque']
+    important_columns = ['mileage', 'engine', 'max_power']
     df = df[df[important_columns].isnull().sum(axis = 1) < 3]           #CONG NULL THEO HANG
 
-    for column in ['mileage', 'engine', 'max_power']:
-        df[column] = df[column].fillna(df[column].median())             #DIEN O NULL BANG TRUNG VI
+    columns = ['mileage','engine','max_power']
+    for column in columns:
+        df.loc[:, column] = df[column].fillna(df[column].median())  # DIEN O NULL BANG TRUNG VI
+    return df
 
-    df['seats'] = df['seats'].fillna(df['seat'].mode()[0])
+def handle_duplicate(df):
+    df = df.drop_duplicates(keep = 'first')
+    return df
+
+def valid_value(df):
+    columns = ['mileage', 'engine', 'max_power','seats']
+    for column in columns:
+        df = df[df[column] > 0]
+    return df
 
 def main():
     df = pd.read_csv('../data/raw/Car details v3.csv')
-    extract_numeric(df)
+    df = extract_numeric(df)
+    df =change_types(df)
+    df = handle_missing(df)
+    df = handle_duplicate(df)
+    df = valid_value(df)
+    df.to_csv('../data/processed/clean_Car_details.csv', index = False)
+    #pd.set_option('display.max_columns', None)
+    #print(df.head(-10))
+    #print(df[['fuel','mileage_unit']].head(10))
 if __name__ == '__main__':
     main()
